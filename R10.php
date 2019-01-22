b3ae9cd1b308f0723d452d717a4433e015191281
Omar <44298100+omards8@users.noreply.github.com>
2019-01-22 13:21:45 +0100

    Add files via upload

---
+++ R10.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Menu de Navegacion</title>
+    <link rel="stylesheet" href="estilos.css"
+  </head>
+  <body>
+    <ul>
+    <?php
+      $menu=[
+        "Inicio",
+        "Blog",
+        "Videos",
+        "Contacto"
+    ];
+     ?>
+    <li><a href="inicio.asp">Inicio</a></li>
+     <li><a href="Blog.asp">Blog</a></li>
+     <li><a href="videos.asp">Videos</a></li>
+     <li><a href="contacto.asp">Contacto</a></li>
+     </ul>
+  </body>
+</html>
